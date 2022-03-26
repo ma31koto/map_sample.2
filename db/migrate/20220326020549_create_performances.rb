0e4d75a043f964f2e5064cc2c69ad50a1a7b8182
EC2 Default User <ec2-user@ip-172-31-22-150.ec2.internal>
2022-03-26 07:09:00 +0000

    first

---
+++ db/migrate/20220326020549_create_performances.rb
@@ -0,0 +1,10 @@
+class CreatePerformances < ActiveRecord::Migration[6.1]
+  def change
+    create_table :performances do |t|
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+      t.timestamps
+    end
+  end
+end
